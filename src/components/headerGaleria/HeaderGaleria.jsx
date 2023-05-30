72142a6d7d1322ab489f301e86315f806feef365
Esteark <eardila1409@gmail.com>
2023-05-30 15:01:33 -0500

    Add changes galeria

--- src/components/headerGaleria/HeaderGaleria.jsx
+++ src/components/headerGaleria/HeaderGaleria.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./stylesHeaderGal.scss";
+import imgHeader1 from ".././../assets/img/galeria/header1.jpg";
+import imgHeader2 from ".././../assets/img/galeria/header2.jpg";
 
 const HeaderGaleria = () => {
   return (
     <section className="headerGal">
       <article className="headerGal-sec1"></article>
       <article className="headerGal-sec2">
-        <figure className="img1"></figure>
-        <figure className="img2"></figure>
+        <figure className="img1">
+          <img src={imgHeader1} alt="" />
+        </figure>
+        <figure className="img2">
+          <img src={imgHeader2} alt="" />
+        </figure>
       </article>
       <article className="headerGal-sec3">
         <h1>MAKING ART</h1>
